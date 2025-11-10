bc315e76efb03c488cff7dff034a72e6826644c2
Alexander Kern <alexander.t.kern@gmail.com>
2025-11-09 23:19:42 -0600

    movement, jumping and shoot input

--- scenes/entities/player.gd
+++ scenes/entities/player.gd
@@ -1,11 +1,26 @@
 extends CharacterBody2D
 
-var direction: Vector2
-var speed = 50
+var direction_x: float
+var speed = 120
+@export var gravity = 10
+@export var jump_strength = 10
 
-func _physics_process(_delta):
-	if Input.is_action_pressed("right"): 
-		direction = Vector2.RIGHT
-		velocity = direction * speed
+
+func get_input(): 
+	direction_x = Input.get_axis("left", "right")
+	if Input.is_action_just_pressed("jump"): 
+		velocity.y -= jump_strength
+	if Input.is_action_just_pressed("shoot") and $ReloadTimer.time_left == 0:
+		print('shooting')
+		$ReloadTimer.start() 
+		
+
+func _physics_process(delta):
+		get_input()
+		velocity.x = direction_x * speed
+		apply_gravity(delta)
 		move_and_slide()
 		
+func apply_gravity(delta): 
+	velocity.y += gravity * delta
+	
