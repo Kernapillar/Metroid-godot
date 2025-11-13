f3bb5065a5a31e96fe5b17759dc9e060bb4e1242
Alexander Kern <alexander.t.kern@gmail.com>
2025-11-12 21:22:45 -0600

    Target icon and Tween implementation

--- scenes/Projectiles/bullet.gd
+++ scenes/Projectiles/bullet.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 var direction: Vector2
-var speed = 200
+var speed = 250
 
 func setup(pos: Vector2, dir: Vector2): 
 	position = pos + dir * 15
@@ -9,3 +9,7 @@ func setup(pos: Vector2, dir: Vector2):
 	
 func _physics_process(delta):
 	position += direction * speed * delta
+func _ready(): 
+	#$Sprite2D.scale = Vector2.ZERO
+	var tween = get_tree().create_tween()
+	tween.tween_property($Sprite2D, "scale", Vector2(1, 1), 0.3).from(Vector2.ZERO)
--- scenes/entities/player.gd
+++ scenes/entities/player.gd
@@ -6,14 +6,14 @@ var speed = 120
 @export var jump_strength = 10
 signal shoot(pos: Vector2, dir: Vector2)
 var aim_directions = {
-	"(1.0, 0.0)": 0, 
-	"(1.0, 1.0)": 1, 
-	"(0.0, 1.0)": 2, 
-	"(-1.0, 1.0)": 3, 
-	"(-1.0, 0.0)": 4, 
-	"(-1.0, -1.0)": 5, 
-	"(0.0, -1.0)": 6, 
-	"(1.0, -1.0)": 7, 
+	"(1, 0)": 0, 
+	"(1, 1)": 1, 
+	"(0, 1)": 2, 
+	"(-1, 1)": 3, 
+	"(-1, 0)": 4, 
+	"(-1, -1)": 5, 
+	"(0, -1)": 6, 
+	"(1, -1)": 7, 
 }
 
 
@@ -24,6 +24,9 @@ func get_input():
 	if Input.is_action_just_pressed("shoot") and $ReloadTimer.time_left == 0:
 		shoot.emit(position, get_local_mouse_position().normalized())
 		$ReloadTimer.start() 
+		var tween = get_tree().create_tween()
+		tween.tween_property($targetMarker, "scale", Vector2(0.1, 0.1), 0.2)
+		tween.tween_property($targetMarker, "scale", Vector2(0.5, 0.5), 0.4)
 		
 
 func _physics_process(delta):
@@ -32,6 +35,7 @@ func _physics_process(delta):
 		apply_gravity(delta)
 		animation()
 		move_and_slide()
+		update_marker()
 		
 func apply_gravity(delta): 
 	velocity.y += gravity * delta
@@ -45,5 +49,8 @@ func animation():
 	if not is_on_floor(): 
 		$AnimationPlayer.current_animation = "jump"
 	var raw_dir = get_local_mouse_position().normalized()
-	var adjusted_dir = Vector2(round(raw_dir.x), round(raw_dir.y))
+	var adjusted_dir = Vector2i(round(raw_dir.x), round(raw_dir.y))
 	$torso.frame = aim_directions[str(adjusted_dir)]
+
+func update_marker(): 
+	$targetMarker.position = get_local_mouse_position().normalized() * 60
